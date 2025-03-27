34b08de9426244abef555d5ab991d097b4f77cc5
rkohler <roger.kohlerjr@gmail.com>
2025-03-27 06:52:08 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from event-based-svcs!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ projects/auth-svc/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from auth-svc!")
+
+
+if __name__ == "__main__":
+    main()
